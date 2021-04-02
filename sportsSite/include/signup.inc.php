d012b6de3196d79ff1cb7caa3eec36345b0f9627
Hassan-Brar <hassanbrar@gmail.com>
2021-04-02 00:23:36 -0700

    Changed html file to php

--- sportsSite/include/signup.inc.php
+++ sportsSite/include/signup.inc.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>User Sign up</title>
-</head>
-<body>
-<h1></h1>
-
-
-
-</body>
-</html>
-
 <?php
 
 
---
+++ sportsSite/signup.php
@@ -0,0 +1,24 @@
+<?php
+    include "navFooter.html"
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Sign up</title>
+    <script> 
+        $(function(){
+          $("#includedContent").load("navFooter.html"); 
+        });
+    </script> 
+</head>
+<body>
+    <div id="includedContent"></div>
+    <h1>Registration</h1>
+    <label for="firstname"><b>First Name</b></label>
+    <input type="text" name="firstname" required>
+
+
+
+</body>
+</html>
