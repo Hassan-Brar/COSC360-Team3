b622f274a5b20cc51c9b3c48e280faecefdd7f30
Nicholas McGee <nicholas.r.mcgee@gmail.com>
2021-04-09 22:02:41 -0600

    Updated SQL

--- Blog.sql
+++ Blog.sql
@@ -1,9 +1,36 @@
-CREATE TABLE user(
+CREATE TABLE Users (
     firstname VARCHAR(25),
     lastname VARCHAR(25),
     username VARCHAR(20),
     pass VARCHAR(255),
+    profileImage BLOB,
     isAdmin BOOLEAN,
-    PRIMARY KEY (userName)
+    PRIMARY KEY (username)
+);
 
+CREATE TABLE Blogs (
+    blogID INT,
+    blogName VARCHAR(50),
+    blogText VARCHAR(MAX),
+    blogImage BLOB,
+    uploadDate DATETIME,
+    likes INT,
+    views INT,
+    PRIMARY KEY(blogID)
+);
+
+CREATE TABLE Comments (
+    blogID INT,
+    username VARCHAR(20),
+    commentText VARCHAR(1000),
+    FOREIGN KEY (username) REFERENCES Users(username),
+    FOREIGN KEY (blogID) REFERENCES Blogs(blogID)
+);
+
+CREATE TABLE Likes (
+    liked BOOLEAN,
+    username VARCHAR(20),
+    blogID INT,
+    FOREIGN KEY (username) REFERENCES Users(username),
+    FOREIGN KEY (blogID) REFERENCES Blogs(blogID)
 );
